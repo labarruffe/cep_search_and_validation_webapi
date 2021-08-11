8f5153ce6d249696b4f2bb9b12dbfd8208a368a3
labarruffe <lucas_barruffe@hotmail.com>
2021-08-11 12:12:55 -0300

    Document api

--- src/controllers/viacep.controller.ts
+++ src/controllers/viacep.controller.ts
@@ -6,15 +6,60 @@ export class Viacep {
         this.request = request;
     }
 
+    /**
+     *
+     * @api {get} /:cep/address Buscar endereço por CEP
+     * @apiName searchAddress
+     * @apiGroup Viacep
+     * @apiVersion  1.0.0
+     *
+     * @apiParam  {String} cep CEP com 8 dígitos sem caracteres alfanuméricos, especiais
+     *
+     * @apiSuccessExample {json} Success-Response
+     *  200 OK - CEP válido e existente
+     *  {
+     *      "cep": "01001-000",
+     *      "logradouro": "Praça da Sé",
+     *      "complemento": "lado ímpar",
+     *      "bairro": "Sé",
+     *      "localidade": "São Paulo",
+     *      "uf": "SP",
+     *      "ibge": "3550308",
+     *      "gia": "1004",
+     *      "ddd": "11",
+     *      "siafi": "7107"
+     *  }
+     * 
+     * @apiSuccessExample {json} Success-Response
+     *  200 OK - CEP válido, mas inexistente
+     *  { 
+     *      "message": "O CEP :cep não possui endereço vinculado."
+     *  }
+     * 
+     * @apiErrorExample Error-Response:
+     *  400 Bad Request CEP com formato inválido
+     *     {
+     *       "message": "O CEP :cep está com formato inválido.",
+     *       "details": "O CEP deve possuir 8 dígitos sem caracteres alfanuméricos, especiais e/ou espaço."
+     *     }
+     * 
+     * @apiErrorExample Error-Response:
+     *     Erros com status code não mapeados
+     *     {
+     *       "message": "Erro desconhecido."
+     *     }
+    */
     async searchAddress(cep: string) {
-        let response;
-        const invalidFormat = { message: `O CEP ${cep} está com formato inválido.`, details: `O CEP deve possuir 8 dígitos sem caracteres especiais e/ou espaço.` };
+        let response, status;
+        const invalidFormat = { message: `O CEP ${cep} está com formato inválido.`, details: `O CEP deve possuir 8 dígitos sem caracteres alfanuméricos, especiais e/ou espaço.` };
         try {
             if (!this.validateCep(cep)) {
                 response = invalidFormat;
+                status = 400;
             } else {
                 //TODO: add viacep url in .env
-                const { data } = await this.request.get(`https://viacep.com.br/ws/${cep}/json/`);
+                const { data, status: resStatus } = await this.request.get(`https://viacep.com.br/ws/${cep}/json/`);
+                status = resStatus;
                 if (data.erro) {
                     response = { message: `O CEP ${cep} não possui endereço vinculado.` }
                 } else {
@@ -22,15 +67,15 @@ export class Viacep {
                 }
             }
         } catch (error) {
-            console.log(error);
-            const { status } = error.response;
-            if (status == 400) {
+            const { status: resStatus } = error.response;
+            status = resStatus;
+            if (resStatus == 400) {
                 response = invalidFormat;
             } else {
                 response = { message: `Erro desconhecido.` };
             }
         }
-        return response;
+        return { response, status };
     }
 
     validateCep(cep: string) {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,8 +5,8 @@ const router = Router();
 const routeSetup = (viacepWebservice: any) => {
     router.get('/:cep/address', async (req: Request, res: Response) => {
         const { cep } = req.params;
-        const response = await viacepWebservice.searchAddress(cep);
-        res.json(response);
+        const { response, status } = await viacepWebservice.searchAddress(cep);
+        res.status(status).json(response);
     });
 
     return router;
