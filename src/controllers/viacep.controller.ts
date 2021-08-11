9cd818a6fc0bd7e39b8cf4eac29b43936313d463
labarruffe <lucas_barruffe@hotmail.com>
2021-08-11 10:14:53 -0300

    Validate cep input in request

--- src/controllers/viacep.controller.ts
+++ src/controllers/viacep.controller.ts
@@ -8,22 +8,33 @@ export class Viacep {
 
     async searchAddress(cep: string) {
         let response;
+        const invalidFormat = { message: `O CEP ${cep} está com formato inválido.`, details: `O CEP deve possuir 8 dígitos sem caracteres especiais e/ou espaço.` };
         try {
-            //TODO: add viacep url in .env
-            const { data } = await this.request.get(`https://viacep.com.br/ws/${cep}/json/`);
-            if (data.erro) {
-                response = { message: `O CEP ${cep} não possui endereço vinculado.` }
+            if (!this.validateCep(cep)) {
+                response = invalidFormat;
             } else {
-                response = data;
+                //TODO: add viacep url in .env
+                const { data } = await this.request.get(`https://viacep.com.br/ws/${cep}/json/`);
+                if (data.erro) {
+                    response = { message: `O CEP ${cep} não possui endereço vinculado.` }
+                } else {
+                    response = data;
+                }
             }
         } catch (error) {
+            console.log(error);
             const { status } = error.response;
             if (status == 400) {
-                response = { message: `O CEP ${cep} está com formato inválido.`, details: `O CEP deve possuir 8 dígitos sem caracteres especiais e/ou espaço.` };
+                response = invalidFormat;
             } else {
                 response = { message: `Erro desconhecido.` };
             }
         }
         return response;
+    }
+
+    validateCep(cep: string) {
+        const cepPattern = /^[0-9]{8}$/;
+        return cepPattern.test(cep);
     }
 }
